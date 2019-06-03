01e519ab9dc7812aa533f6dd85b6683baa4d1bc2
jotarios <j29.rios@gmail.com>
2019-06-03 10:00:47 -0500

    Add consolidate conditional expression

--- src/simplifying_conditional_expressions/consolidate-conditional-expression_before.java
+++ src/simplifying_conditional_expressions/consolidate-conditional-expression_before.java
@@ -1,13 +1,8 @@
 double disabilityAmount() {
-  if (seniority < 2) {
-    return 0;
+  if (seniority < 2 || monthsDisabled > 12 || isPartTime) {
+      return 0;
   }
-  if (monthsDisabled > 12) {
-    return 0;
-  }
-  if (isPartTime) {
-    return 0;
-  }
+
   // Compute the disability amount.
   // ...
 }
